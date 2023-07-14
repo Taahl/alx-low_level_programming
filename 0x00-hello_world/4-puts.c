d2772cf3a90bbb0829cd9f79763c864920aef15e
taahl <taha_l@hotmail.fr>
2023-07-13 17:12:46 -0700

    Please

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 /**
- * Main - program that prints a sentence 
+ * main - Entry point print sentence
  *
  * Return: Always 0 (Success)
  */
@@ -9,3 +9,4 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+
