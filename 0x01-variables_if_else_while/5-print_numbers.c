2bf2b85cf76ea55de79d6874fd6e4d89cfcad49a
taahl <taha_l@hotmail.fr>
2023-07-14 18:39:16 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print single digit numbers base 10
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
