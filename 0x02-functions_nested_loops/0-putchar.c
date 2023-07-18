824afbe5e73d5fb3cdba30ce87cd9e0d4a695e3e
taahl <taha_l@hotmail.fr>
2023-07-18 15:20:18 -0700

    putchar no loop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,19 @@
-int _putchar(char c);
+#include "main.h"
+/**
+ * main - prints putchar
+ *
+ * return: 0 (Success)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\');
+	return (0)
+}
