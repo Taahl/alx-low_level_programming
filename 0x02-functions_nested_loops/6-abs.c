4cb9bbf5323eaf531785dd7602f2fdd9787600de
taahl <taha_l@hotmail.fr>
2023-07-18 16:57:07 -0700

    Absolutely

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - absolute value of an int
+ * @i: input number
+ *
+ * Return: absol
+ */
+int _abs(int i)
+{
+	if (i > 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
