d43196420de244b8f76cbda6c21a6909cb4e2e76
taahl <taha_l@hotmail.fr>
2023-07-18 15:42:21 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - print alphabet in lower case
+ *
+ * Return: 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i >= 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
