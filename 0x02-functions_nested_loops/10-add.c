52207d62ea6fcc69a68078d147fee7353b52f9f0
taahl <taha_l@hotmail.fr>
2023-07-18 18:39:50 -0700

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - addition and then result
+ * @x: first int
+ * @z: second int
+ * Return: sum of a && b
+ */
+int add(int x, int z)
+{
+	int y;
+
+	y = x + z;
+
+	return (y);
+}
